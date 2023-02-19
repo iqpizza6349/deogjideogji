532a5f358b2df9ab0713f664da3fa0dd55b32bcd
iqpizza6349 <iqpizza6349@gmail.com>
2023-02-19 14:58:07 +0900

    :tada: :: Initial commit

---
+++ src/main/java/me/iqpizza6349/deogjideogji/DeogjiDeogjiApplication.java
@@ -0,0 +1,13 @@
+package me.iqpizza6349.deogjideogji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeogjiDeogjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeogjiDeogjiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/iqpizza6349/deogjideogji/DeogjiDeogjiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iqpizza6349.deogjideogji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeogjiDeogjiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
